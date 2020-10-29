60dd41a96db9f160c737a6e76545734212bc2c3e
juprianto33 <71082474+juprianto33@users.noreply.github.com>
2020-10-29 08:50:06 +0700

    penambahan e-commerce

--- resources/views/contact2.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('admin.admin')
-
-@section('content')
-
-    <h1>ini contact</h1>
-@endsection
--- resources/views/product2.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('admin.admin')
-
-@section('content')
-
-    <h1>ini product</h1>
-@endsection
